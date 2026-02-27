42df1f37398ba9ec28ac7b8dbcf967e51ad03483
Agyemang Kwame Antwi <kwameagyemang2007@gmail.com>
2026-02-27 20:03:37 +0000

    Add initial implementation of equality and relational operators in assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,15 @@
+const age = 18;
+const userAge = "18";
+
+// Loose Equality
+console.log("Loose Equality (==): ", userAge == age);
+
+// Strict Equality
+console.log("Strict Equality (===): ", userAge === age);
+
+// Loose Inequality
+console.log("Loose Inequality (!==): ", userAge !== age);
+
+// Relational Operators
+console.log("Is 1 greater than 2? ", 1 > 2);
+console.log("Is 5 less  than or equal to 5? ", 5 <= 5);
